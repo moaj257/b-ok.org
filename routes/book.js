b276eecf30e5ff9e86bf8335ec628101e123c966
moaj257 <moaj257@gmail.com>
2020-06-04 23:29:59 +0530

    multi & attr array working issue - fixed

--- routes/book.js
+++ routes/book.js
@@ -50,7 +50,7 @@ router.get('/search', async function(req, res, next) {
           { selector: '.itemCover a', attr: ['href'], isAttr: true, keyValue: 'link', root: '.resItemBox.resItemBoxBooks', multi: false },
           { selector: 'h3[itemprop="name"]', attr: [], isAttr: false, keyValue: 'title', root: '.resItemBox.resItemBoxBooks', multi: false },
           { selector: 'div[title="Publisher"]', attr: [], isAttr: false, keyValue: 'publiser', root: '.resItemBox.resItemBoxBooks', multi: false },
-          { selector: '.authors a[itemprop="author"]', attr: [], isAttr: false, keyValue: 'authors', root: '.resItemBox.resItemBoxBooks', multi: true },
+          { selector: '.authors a[itemprop="author"]', attr: ['href', 'text'], isAttr: true, keyValue: 'authors', root: '.resItemBox.resItemBoxBooks', multi: true },
           { selector: '.bookDetailsBox .bookProperty.property_year .property_value', attr: [], isAttr: false, keyValue: 'year', root: '.resItemBox.resItemBoxBooks', multi: false },
           { selector: '.bookDetailsBox .bookProperty.property_language .property_value', attr: [], isAttr: false, keyValue: 'language', root: '.resItemBox.resItemBoxBooks', multi: false },
           { selector: '.bookDetailsBox .bookProperty.property__file .property_value', attr: [], isAttr: false, keyValue: 'file', root: '.resItemBox.resItemBoxBooks', multi: false }
